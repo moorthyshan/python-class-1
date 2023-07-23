6c31609f0c1a73ef687ab5a971bf755b4a716de0
moorthyshan <113831655+moorthyshan@users.noreply.github.com>
2023-07-23 23:17:33 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("babu")
+
+
+# In[2]:
+
+
+print("hi \nkodi \tbabu")
+
+
+# In[4]:
+
+
+# print("\t0.5thu")
+# print("hi \nkodi \tbabu")
+
+
+# In[7]:
+
+
+'''
+babu
+kodi
+abi
+'''
+
+
+# In[11]:
+
+
+import logging
+logger= logging.getLogger()
+logger.error("error was occured")
+logger.critical("warining")
+
+
+# In[12]:
+
+
+a="abi loves alamelu "
+# a is string
+# "abi loves alamelu" is the value of a 
+print(a)
+
+
+# In[13]:
+
+
+print(a)
+
+
+# In[20]:
+
+
+a ="tea"
+print(a)
+print(type(a))
+print(isinstance(a,str))
+print(isinstance(a,int))
+
+
+# In[21]:
+
+
+a =1
+print(a)
+print(type(a))
+print(isinstance(a,str))
+print(isinstance(a,int))
+
+
+# In[23]:
+
+
+a="KodiUppuli"
+print(len(a))
+print(sorted(a))
+print(min(a))
+print(max(a))
+
+
+# In[24]:
+
+
+print(a.upper())
+print(a.lower())
+print(a.isupper())
+print(a.islower())
+print(a.isdigit())
+print(a.isalpha())
+print(a.isalnum())
+
+
+# In[ ]:
+
+
+
+
